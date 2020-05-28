3d28b749e07b130150a6319355fb3f8aa16b2a14
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-05-28 20:57:16 -0300

    feat(structure): split the app into multiple files

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,16 @@
+export function AutoBind(
+  _target: any,
+  _methodname: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
