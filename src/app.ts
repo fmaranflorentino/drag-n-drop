08f57657b5c79ef66783357d4f26afb9f9d141f5
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-05-28 16:53:50 -0300

    feat(input-validation): create a validation func

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,41 @@
+// validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable): boolean {
+  let isValid = true;
+  const { value, required, minLength, maxLength, min, max } = validatableInput;
+
+  if (required) {
+    isValid = isValid && value.toString().trim().length !== 0;
+  }
+
+  if (minLength != null && typeof value === 'string') {
+    isValid = isValid && value.length > minLength;
+  }
+
+  if (maxLength != null && typeof value === 'string') {
+    isValid = isValid && value.length < maxLength;
+  }
+
+  if (min != null && typeof value === 'number') {
+    isValid = isValid && value > min;
+  }
+
+  if (max != null && typeof value === 'number') {
+    isValid = isValid && value < max;
+  }
+
+  return isValid;
+}
+
+//decorators
 function AutoBind(
   _target: any,
   _methodname: string | Symbol,
@@ -51,13 +89,56 @@ class ProjectInput {
   }
 
   private configure() {
-    this.element.addEventListener('submit', this.handleFormSubmit)
+    this.element.addEventListener('submit', this.handleFormSubmit);
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const title: Validatable = {
+      value: this.titleEl.value,
+      required: true,
+    };
+    const description: Validatable = {
+      value: this.descriptionEl.value,
+      required: true,
+      minLength: 10,
+      maxLength: 100,
+    };
+    const people: Validatable = {
+      value: +this.peopleEl.value,
+      min: 2,
+      max: 5,
+    };
+
+
+    if (
+      !validate(title) ||
+      !validate(description) ||
+      !validate(people)
+    ) {
+      alert('invalid input');
+      return;
+    } else {
+      return [title.value.toString(), description.value.toString(), +people.value];
+    }
+  }
+
+  private clearInputs() {
+    this.titleEl.value = '';
+    this.descriptionEl.value = '';
+    this.peopleEl.value = '';
   }
 
   @AutoBind
   private handleFormSubmit(e: Event) {
     e.preventDefault();
-    console.log('form', this.titleEl.value);
+
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+
+      this.clearInputs();
+    }
   }
 }
 
