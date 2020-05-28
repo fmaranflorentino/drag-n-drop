da039411f82358348111509af6723e87e1c67516
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-05-28 15:57:20 -0300

    feat(decorators): create autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,27 @@
+function AutoBind(
+  _target: any,
+  _methodname: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleEl: HTMLInputElement;
+  descriptionEl: HTMLTextAreaElement;
+  peopleEl: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>(
@@ -14,11 +34,30 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleEl = <HTMLInputElement>this.element.querySelector('#title');
+    this.descriptionEl = <HTMLTextAreaElement>(
+      this.element.querySelector('#description')
+    );
+    this.peopleEl = <HTMLInputElement>this.element.querySelector('#people');
+
     this.attachTemplate();
+    this.configure();
   }
 
   private attachTemplate() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.handleFormSubmit)
+  }
+
+  @AutoBind
+  private handleFormSubmit(e: Event) {
+    e.preventDefault();
+    console.log('form', this.titleEl.value);
   }
 }
 
